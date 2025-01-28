093a85b681f1dbae030f6ed29ff670c8db42294a
giahur <hurgia21@gmail.com>
2025-01-27 19:24:45 -0500

    reverse hamburger

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    document.getElementById("nav-bar").classList.toggle("active");
-}
-
-const hamburger = document.querySelector('.hamburger-menu');
-hamburger.addEventListener('click', toggleMenu);
