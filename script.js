7bdab8430eeadd94ca463f0de2b21cc88fce26b4
giahur <hurgia21@gmail.com>
2025-01-27 19:08:06 -0500

    hamburger update

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function toggleMenu() {
     document.getElementById("nav-bar").classList.toggle("active");
 }
+
+const hamburger = document.querySelector('.hamburger-menu');
+hamburger.addEventListener('click', toggleMenu);
